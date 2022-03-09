621714bc64ef0f02fea0e40c71de9db9dd79702c
naser-yousef <naser.yousef5@yahoo.com>
2022-03-09 10:38:29 +0300

    first commit

---
+++ src/main/java/com/xocialive/jenkinsdemo/controllers/WebService.java
@@ -0,0 +1,14 @@
+package com.xocialive.jenkinsdemo.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebService {
+
+	@GetMapping("/hello")
+	public String sayHello() {
+		return "hello";
+	}
+
+}
